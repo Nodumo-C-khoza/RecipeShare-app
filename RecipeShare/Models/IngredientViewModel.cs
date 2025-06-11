caf9634470b2230640855873459ba903c8764fa2
nkhoz <nkhoza@rfid.co.za>
2025-06-11 22:26:56 +0200

    update viewmodel to require ingredients

--- RecipeShare/Models/IngredientViewModel.cs
+++ RecipeShare/Models/IngredientViewModel.cs
@@ -5,20 +5,29 @@ namespace RecipeShare.Models
     public class IngredientViewModel
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Amount { get; set; }
-        public string Unit { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
+        [StringLength(100, ErrorMessage = "Name must be less than 100 characters")]
+        public string Name { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Amount is required")]
+        [StringLength(50, ErrorMessage = "Amount must be less than 50 characters")]
+        public string Amount { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Unit is required")]
+        [StringLength(50, ErrorMessage = "Unit must be less than 50 characters")]
+        public string Unit { get; set; } = string.Empty;
     }
 
     public class CreateIngredientViewModel
     {
-        [Required]
-        [StringLength(100)]
-        public string Name { get; set; }
+        [Required(ErrorMessage = "Name is required")]
+        [StringLength(100, ErrorMessage = "Name must be less than 100 characters")]
+        public string Name { get; set; } = string.Empty;
 
-        [Required]
-        [StringLength(50)]
-        public string Amount { get; set; }
+        [Required(ErrorMessage = "Amount is required")]
+        [StringLength(50, ErrorMessage = "Amount must be less than 50 characters")]
+        public string Amount { get; set; } = string.Empty;
 
         [StringLength(20)]
         public string Unit { get; set; }
